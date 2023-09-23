a8066103a3b4f07e836cb0294c111ad038de7dc9
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-23 11:36:46 -0300

    [Add] modelo pokemon

--- src/models/Pokemon.js
+++ src/models/Pokemon.js
@@ -1,12 +1,50 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('pokemon', {
+
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    hp: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    attack: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    defense: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    speed: {
+      type: DataTypes.STRING,
+    },
+
+    height: {
+      type: DataTypes.INTEGER,
+    },
+
+    weight: {
+      type: DataTypes.INTEGER,
+    },
+
   });
 };
