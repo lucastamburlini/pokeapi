80072e6d04d32e3bcbcbd2b57bf3a31f0d7506cd
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-23 12:58:51 -0300

    [fix] arreglos en la configuración de las rutas

--- src/routes/typesRouter.js
+++ src/routes/typesRouter.js
@@ -2,3 +2,12 @@
 Obtiene un arreglo con todos los tipos de pokemones.
 En una primera instancia, cuando la base de datos este vacía, deberás guardar todos los tipos que encuentres en la API.
 Estos deben ser obtenidos de la API (se evaluará que no haya hardcodeo). Luego de obtenerlos de la API, deben ser guardados en la base de datos para su posterior consumo desde allí. */
+
+const { Router } = require("express");
+const typesRouter = Router()
+
+typesRouter.get("/", (req, res) => {
+    res.send("ESTA RUTA TRAE EL TYPO")
+})
+
+module.exports = typesRouter;
