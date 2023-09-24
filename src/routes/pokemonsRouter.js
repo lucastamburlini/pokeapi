693f0154cfdf3cd36441bd1a0ee3cd2801b8c7c4
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-09-24 18:34:05 -0300

    .

--- src/routes/pokemonsRouter.js
+++ src/routes/pokemonsRouter.js
@@ -2,15 +2,18 @@ const { Router } = require("express");
 const pokemonsRouter = Router()
 
 pokemonsRouter.get("/", (req, res) => {
-    res.send("NIY: ESTA RUTA TRAE A TODOS LOS POKE")
+    const { name } = req.query;
+    if (name !== undefined) res.send(`NIY: ESTA RUTA TRAE NAME:${name}`)
+    else res.send("NIY: ESTA RUTA TRAE A TODOS LOS POKE")
 });
 
-pokemonsRouter.get("/:idPokemon", (req, res) => {
-    res.send("NIY: ESTA RUTA TRAE INFO DE UN POKE")
+pokemonsRouter.get("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(`NIY: ESTA RUTA TRAE INFO DE UN POKE CON ID ${id}`)
 });
 
-pokemonsRouter.get("/name", (req, res) => {
-    res.send("NIY: ESTA RUTA TRAE INFO DE UN POKE")
+pokemonsRouter.post("/pokemons", (req, res) => {
+    res.send("NIY: ESTA RUTA TRAE LOS DATOS PARA CREAR UN POKEMON")
 });
 
 module.exports = pokemonsRouter;
