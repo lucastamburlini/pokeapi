e97173b1887b26f9ce4bd7ec1e85659e23d6d199
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-10-08 00:08:30 -0300

    [Add] test

--- tests/index.test.js
+++ tests/index.test.js
@@ -16,8 +16,9 @@ describe("Route test", () => {
             await agent.get("/pokemons/b5a1dbb9-60ac-49cf-b754-00f2cd12a9a1").expect(200)
         }, 10000)
 
-        it('Name pokemon: Responds with status: 200', async () => {
-            await agent.get("/pokemons/name").expect(200)
+        it('Name pokemon: Responds with status: 200 and contains a Pokemon name', async () => {
+            const response = await agent.get("/pokemons/?name=pikachu").expect(200);
+            expect(response.request.url).toMatch(/name=pikachu/);
         }, 10000)
 
         it('Created pokemons: Responds with status: 200', async () => {
