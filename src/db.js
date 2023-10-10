6a6a60aa0cbae436c1d6ea4291164136ca2cebd6
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-10-10 10:53:37 -0300

    .

--- src/db.js
+++ src/db.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { Pool } = require('pg');
 // const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const PokemonModel = require("./models/Pokemon");
 const TypeModel = require("./models/Type");
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
-   logging: false,
-   native: false
- });
+const sequelize = new Sequelize(
+   "postgres://default:sCmoLgQbP5y4@ep-square-shadow-40194873-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb",
+   {
+      logging: false,
+      native: false
+   }
+); 
 
 /* const sequelize = new Sequelize(
    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
