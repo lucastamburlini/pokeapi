3ccedebf2e8a51d09fc22aa449f563242ac8c2f4
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-10-10 11:46:24 -0300

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT =  process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
+  server.listen(PORT,"0.0.0.0", () => {
     console.log(`Escuchando el puerto ${PORT}`); // eslint-disable-line no-console
   });
 });
