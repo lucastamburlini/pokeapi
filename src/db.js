af99e48c0237b92016f25126a996aebbd2e9e49e
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-10-10 10:50:04 -0300

    .

--- src/db.js
+++ src/db.js
@@ -1,24 +1,18 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const { Pool } = require('pg');
 // const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const PokemonModel = require("./models/Pokemon");
 const TypeModel = require("./models/Type");
-
-import pg from 'pg';
-
-const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
 
-const sequelize = new Sequelize(
-   pool,
-   {
-      logging: false,
-      native: false
-   }
-); 
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+   logging: false,
+   native: false
+ });
 
 /* const sequelize = new Sequelize(
    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
