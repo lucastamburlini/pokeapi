e80f8b08ee439f7b8db0dc2ed55175379e4dc4bd
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-10-10 16:02:05 -0300

    .

--- src/db.js
+++ src/db.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 const PokemonModel = require("./models/Pokemon");
 const TypeModel = require("./models/Type");
+const pg = require('pg');
 
 /* const sequelize = new Sequelize(
    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
@@ -16,7 +17,8 @@ const sequelize = new Sequelize(
    DB_DEPLOY,
    {
       logging: false,
-      native: false
+      native: false,
+      dialectModule: pg
    }
 );
 
